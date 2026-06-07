45daa9896925fb3fb55ac4aff74b40ececa22a92
ZPeppler <peppler.zachary@gmail.com>
2026-06-07 15:23:58 -0400

    Initial setup

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  outputs =
+    {
+      self,
+      nixpkgs,
+      ...
+    }:
+    let
+      systems = "x86_64-linux";
+    in
+    {
+      packages = builtins.listToAttrs (
+        map (
+          system:
+          let
+            pkgs = import nixpkgs { inherit system; };
+          in
+          {
+            name = system;
+            value = rec {
+              shellenv = pkgs.buildEnv {
+                name = "shellenv";
+
+                paths = [
+                  pkgs.zsh
+                ];
+              };
+
+              default = shellenv;
+            };
+          }
+        ) systems
+      );
+    };
+}
+
